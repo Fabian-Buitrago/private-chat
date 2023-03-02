7c1956d840d0684e6b77f4b1965add87ff5a98fc
fbuitrago <fabianbuitragov@gmail.com>
2023-03-02 18:34:43 -0500

    Add search component and styles

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Search = () => {
-  return <div>Search</div>;
+  return (
+    <div className="search">
+      <div className="searchForm">
+        <input type="text" placeholder="Find a user" />
+      </div>
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Jane</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
