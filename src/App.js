0f8f977fbb3cca7294dda3d5af2513c76dabb22c
fbuitrago <fabianbuitragov@gmail.com>
2023-03-02 13:47:54 -0500

    Add home components without styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./style.scss";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 function App() {
-  return <Login />;
+  return <Home />;
 }
 
 export default App;
---
+++ src/components/Chats.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Chats = () => {
+  return <div className="chat">Chats</div>;
+};
+
+export default Chats;
