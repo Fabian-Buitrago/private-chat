f76accc22f74c120b30532778b99e7b10a9dcd46
fbuitrago <fabianbuitragov@gmail.com>
2023-03-02 19:07:45 -0500

    Create chats, update sidebar and search components

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,7 +1,31 @@
 import React from "react";
 
 const Chats = () => {
-  return <div className="chats">Chats</div>;
+  return (
+    <div className="chats">
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>Hello</p>
+        </div>
+      </div>
+
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chats;
