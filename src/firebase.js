0ce4ad930e58db0aadaa96e2ab34baf1efa38075
fbuitrago <fabianbuitragov@gmail.com>
2023-03-15 16:16:57 -0500

    Remove credentials

--- src/firebase.js
+++ src/firebase.js
@@ -3,22 +3,13 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
-// const firebaseConfig = {
-//   apiKey: process.dev.REACT_APP_API_KEY,
-//   authDomain: process.dev.REACT_APP_AUTH_DOMAIN,
-//   projectId: process.dev.REACT_APP_PROJECT_ID,
-//   storageBucket: process.dev.REACT_APP_STORAGE_BUCKET,
-//   messagingSenderId: process.dev.REACT_APP_MESSAGING_SENDER_ID,
-//   appId: process.dev.REACT_APP_API_ID
-// }
-
 const firebaseConfig = {
-  apiKey: 'AIzaSyCnxPO8ldLgV5G2eLqh80AxHrgAFaA9PEw',
-  authDomain: 'chat-824f3.firebaseapp.com',
-  projectId: 'chat-824f3',
-  storageBucket: 'chat-824f3.appspot.com',
-  messagingSenderId: '328883950866',
-  appId: '1:328883950866:web:1dd6c3e4511e060c668fcb'
+  apiKey: process.dev.REACT_APP_API_KEY,
+  authDomain: process.dev.REACT_APP_AUTH_DOMAIN,
+  projectId: process.dev.REACT_APP_PROJECT_ID,
+  storageBucket: process.dev.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.dev.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.dev.REACT_APP_API_ID
 }
 
 // Initialize Firebase
