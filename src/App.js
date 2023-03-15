77146fcc8cc21b16a619dc58cf2fee164d771324
fbuitrago <fabianbuitragov@gmail.com>
2023-03-15 14:42:50 -0500

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import './style.scss'
+import Login from './pages/Login'
+import Home from './pages/Home'
 import Register from './pages/Register'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App () {
-  return <Register />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' />
+        <Route index element={<Home />} />
+        <Route path='login' element={<Login />} />
+        <Route path='register' element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -20,20 +20,34 @@ const Register = () => {
 
       const uploadTask = uploadBytesResumable(storageRef, file)
 
-      uploadTask.on(
+      uploadTask.on('state_changed',
+        (snapshot) => {
+        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+          console.log('Upload is ' + progress + '% done')
+          switch (snapshot.state) {
+            case 'paused':
+              console.log('Upload is paused')
+              break
+            case 'running':
+              console.log('Upload is running')
+              break
+          }
+        },
         (error) => {
-          console.log(error)
-          setErr(true)
+          console.log(error.code)
         },
         () => {
           getDownloadURL(uploadTask.snapshot.ref).then(async (photoURL) => {
             await updateProfile(res.user, { displayName, photoURL })
-
             await setDoc(doc(db, 'users', res.user.uid), {
               uid: res.user.uid,
               displayName,
               email,
               photoURL
+            })
+
+            await setDoc(doc(db, 'userChats', res.user.uid), {
             })
           })
         }
